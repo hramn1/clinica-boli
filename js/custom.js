426ae5046567fbb1cfb64103f53a8bcca04cd740
Unknown <stmihailov@yandex.ru>
2018-02-05 02:01:50 +0300

    правки

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,27 @@
   var menuCanvas = document.querySelector(".main-menu__offcanvas");
   var feature = document.querySelectorAll(".feature__item");
   var menuClose = document.querySelector(".close-cross");
+  var menuDrop = document.querySelector(".main-menu__dropdown");
+  var menuShow = document.querySelector(".drop-menu");
+  var menuDropTop = 0;
+  var timer;
+  menuDrop.addEventListener('mouseover', function(evt){
+     
+      var showingMenu = function(){
+        if(menuDropTop < 100){
+          menuDropTop = menuDropTop + 5;
+      menuShow.style.height = menuDropTop + 'px';
+      timer = setTimeout(showingMenu, 20);
+      console.log(menuDropTop);
+      }};
+      showingMenu();
+       
+  });
+  menuDrop.addEventListener('mouseout', function(evt){
+    menuShow.style.height = 0;
+    menuDropTop = 0;
+    //clearTimeout(timer);
+  });
   menuButton.addEventListener('click', function(evt){
     menuCanvas.classList.add('main-menu__offcanvas--show');
   });
