301f8e818465ca8e53b52c50fdf688fe3a649ec0
IT <stmihailov@andex.ru>
2018-02-05 19:18:23 +0300

    правки

--- gulpfile.js
+++ gulpfile.js
@@ -105,3 +105,51 @@ gulp.task("copy", function() {
 gulp.task("clean", function() {
   return del("build");
 });
+
+gulp.task("css-lint", function() {
+
+  // Stylelint config rules
+  var stylelintConfig = {
+    "rules": {
+      "block-no-empty": true,
+      "color-no-invalid-hex": true,
+      "declaration-colon-space-after": "always",
+      "declaration-colon-space-before": "never",
+      "function-comma-space-after": "always",
+      "function-url-quotes": "double",
+      "media-feature-colon-space-after": "always",
+      "media-feature-colon-space-before": "never",
+      "media-feature-name-no-vendor-prefix": true,
+      "max-empty-lines": 5,
+      "number-leading-zero": "never",
+      "number-no-trailing-zeros": true,
+      "property-no-vendor-prefix": true,
+      "rule-no-duplicate-properties": true,
+      "declaration-block-no-single-line": true,
+      "rule-trailing-semicolon": "always",
+      "selector-list-comma-space-before": "never",
+      "selector-list-comma-newline-after": "always",
+      "selector-no-id": true,
+      "string-quotes": "double",
+      "value-no-vendor-prefix": true
+    }
+  }
+
+  var processors = [
+    stylelint(stylelintConfig),
+    // Pretty reporting config
+    reporter({
+      clearMessages: true,
+      throwError: true
+    })
+  ];
+
+  return gulp.src(
+      // Stylesheet source:
+      ['app/assets/css/**/*.css',
+      // Ignore linting vendor assets:
+      // (Useful if you have bower components)
+      '!app/assets/css/vendor/**/*.css']
+    )
+    .pipe(postcss(processors));
+});
